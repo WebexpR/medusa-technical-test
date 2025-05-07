4156a94c5b3a3d471f29976a0d4575247768cd24
Adil <abasri@webexpr.dev>
2025-05-07 14:29:05 +0200

    feat: Implement wishlist table schema in migration

--- src/migrations/1744797048433-add-wishlist-schema.ts
+++ src/migrations/1744797048433-add-wishlist-schema.ts
@@ -12,6 +12,17 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class AddWishlistSchema1744797048433 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        // Example with Wishlist Table
+        await queryRunner.query(`
+            CREATE TABLE "wishlist" (
+                "id" character varying NOT NULL,
+                "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+                "title" character varying NOT NULL,
+                "customer_id" character varying NOT NULL,
+                PRIMARY KEY ("id")
+            )
+        `)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
